0fbd246b3b67ed76031321fd8638b31426e49fa0
jin as root <jcguu95@gmail.com>
2019-12-01 13:41:02 -0500

    this shitty project has been terminated. code is too dirty..

--- main.sh
+++ main.sh
@@ -1,20 +1,9 @@
 #!/bin/bash
 ## make sure your bash is after v4.0 for `readarray` to work.
 
-
-
-mainDir="/root/curfew" ; mkdir $mainDir/log $mainDir/var ;
-
-
-
-# LOGGING #
-## Everything below will go to the file '$logfile':
-logfile=$mainDir/log/$(date "+%Y-%m-%dT%H:%M:%S%z").txt
-exec 3>&1 4>&2
-trap 'exec 2>&4 1>&3' 0 1 2 3 RETURN
-exec 1>$logfile 2>&1
-
-
+mainDir="/root/curfew" 
+[[ -e "$mainDir/log" ]] || mkdir $mainDir/log 
+[[ -e "$mainDir/var" ]] || mkdir $mainDir/var
 
 # MODULAR CONFIGS #
 ## readarray is supported only after bash 4.0
@@ -36,13 +25,14 @@ indulgenceDir="$mainDir/var/indulgence"
 indulgenceTodayFile="$indulgenceDir/$(date +"%Y%m%d").txt"
 indulgenceDefaultAmount=600
 
-mkdir $indulgenceDir ; touch $indulgenceTodayFile
-if [[ -s $indelgenceTodayFile ]]; then
-	echo "message: indulgence file has been here. do nothing."
+[[ -e $indulgenceDir ]] || mkdir $indulgenceDir
+touch $indulgenceTodayFile
+
+if [[ -s $indulgenceTodayFile ]]; then
+	echo -e "message: indulgenceTodayFile is not empty. This step does nothing.\n"
 else	echo "indulgenceAmount=$indulgenceDefaultAmount" >> "$indulgenceTodayFile"
 	echo "indulgenceMode="off"" >> "$indulgenceTodayFile"
 fi
-
 
 
 # Auxiliary functions #
@@ -107,9 +97,12 @@ fi
 	toggle_indulgence() {
 		source $indulgenceTodayFile
 		if [[ $indulgenceAmount -lt 0 ]]; then
-			echo "indulgenceMode="off""
+			echo "indulgenceMode="off"" >> $indulgenceTodayFile
 		else
-			[[ $indulgenceMode == "on" ]] && echo "indulgenceMode="off"" || echo "indulgenceMode="on""
+			if [[ $indulgenceMode == "on" ]]; then
+				echo "indulgenceMode="off"" >> $indulgenceTodayFile
+			else 	echo "indulgenceMode="on""  >> $indulgenceTodayFile
+			fi
 		fi
 	}
 
@@ -117,56 +110,81 @@ fi
 # MAIN #
 
 main() {
-	if [[ ! -z $1 ]]; then
-		[[ $1 == "--indulgence" ]] && toggle_indulgence
-		cat $indulgenceTodayFile
-	fi
-
-
-
-	while [[ -z $1 ]]; do 								# While no argument.. do this infinite loop.
-		echo "current: $(date "+%Y-%m-%dT%H:%M:%S%z")"
-		now=$(date +"%H%M%S") ; sleep 5 ;
-
-		# if [[ $(date +%u) -lt 7 ]] ; then 					# restrictions on non-Sundays
-		if [[ $(date +%u) -lt 8 ]] ; then 					# restrictions everyday!
-
-			source $indulgenceTodayFile
-			if [[ $indulgenceMode == "on" ]] ; then
-				echo "message: indulgence-mode on; no restrictions impulsed"
-				echo "indulgenceAmount=$(expr $indulgenceAmount -5)" >> "$indulgenceTodayFile"
-
-			elif $(if_within $FunTimeStarts $FunTimeEnds) ; then		# during fun-time
-				echo "message: fun-time on"
-				unban_url
-			elif $(if_within $MidnightWorkStarts $MidnightWorkEnds)	; then	# during midnight-work-time
-				echo "message: midnightwork-time on"
-				unban_url
-			else								# during neither
-				echo "message: neither fun-time nor midnightwork-time is on"
-				ban_url
-				kill_app
-			fi
+	if [[ ! -z $1 ]]; then		# interactive mode
+		## Greeting
+		echo -e "message: This is indulgence-toggle-mode."
+		[[ "$1" == "--indulgence" ]] && toggle_indulgence || echo "exit: wrong argument"
 		
+		## Report
+		source $indulgenceTodayFile
+		echo -e "message:    ==Report=="
+		echo -e "message: indulgenceMode   = $indulgenceMode"
+		echo -e "message: indulgenceAmount = $indulgenceAmount (seconds left for today.)\n"
 
+		echo -e "message: exiting..."
+		exit
+	else				# normal curfew mode
+		# LOGGING #
+		## Everything below will go to the file '$logfile':
+		logfile=$mainDir/log/$(date "+%Y-%m-%dT%H:%M:%S%z").txt
+		exec 3>&1 4>&2
+		trap 'exec 2>&4 1>&3' 0 1 2 3 RETURN
+		exec 1>$logfile 2>&1
+
+		while true; do 									# While no argument.. do this infinite loop.
+			echo "timenow: $(date "+%Y-%m-%dT%H:%M:%S%z")"
+			now=$(date +"%H%M%S") ; sleep 5 ;
+
+			# if [[ $(date +%u) -lt 7 ]] ; then 					# restrictions on non-Sundays
+			if [[ $(date +%u) -lt 8 ]] ; then 					# restrictions everyday!
+
+
+				source $indulgenceTodayFile ; source $requestFile ; 
+				if   [[ $request == "on" && $indulgenceAmount -gt 0 ]] ; then
+					echo "message: request for indulgence -- proceed!"
+					toggle_indulgence on
+				elif [[ $request == "on" && $indulgenceAmount -le 0 ]] ; then
+					echo "message: request for indulgence but has run out of balance"
+				else [[ $request == "off" ]] 
+					echo "message: no request for indulgence"
+				fi
+
+				if [[ $indulgenceMode == "on" ]] ; then
+					echo "message: indulgence-mode on; no restrictions impulsed"
+					echo "indulgenceAmount=$(( $indulgenceAmount - 5 ))" >> "$indulgenceTodayFile"
+
+				elif $(if_within $FunTimeStarts $FunTimeEnds) ; then		# during fun-time
+					echo "message: fun-time on"
+					unban_url
+				elif $(if_within $MidnightWorkStarts $MidnightWorkEnds)	; then	# during midnight-work-time
+					echo "message: midnightwork-time on"
+					unban_url
+				else								# during neither
+					echo "message: neither fun-time, indulgence-mode nor midnightwork-time is on"
+					ban_url
+					kill_app
+				fi
 			
-			if $(if_within $CurfewStarts $CurfewEnds) ; then		# during curfew
-				echo "message: curfew on"
-				suspend_user
-			else								# not during curfew
-				echo "message: curfew off"
-				continue_user
+
+				
+				if $(if_within $CurfewStarts $CurfewEnds) ; then		# during curfew
+					echo "message: curfew on"
+					suspend_user
+				else								# not during curfew
+					echo "message: curfew off"
+					continue_user
+				fi
+
+			# 	else								# no restrictions on Sundays
+			# 	echo "message: no restriction for Sundays."
+			# 	unban_url
+			# 	continue_user
+
 			fi
-
-		# 	else								# no restrictions on Sundays
-		# 	echo "message: no restriction for Sundays."
-		# 	unban_url
-		# 	continue_user
-
-		fi
-	done
+		done
+	fi
 }
 
-main
+main $@
 
 exit
