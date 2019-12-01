fdd255199e8577b5d61cbdf7fbfe6e0fcfa902d6
jin as root <jcguu95@gmail.com>
2019-12-01 12:00:52 -0500

    finish indulgence mode ver 1... what a shitty code

--- main.sh
+++ main.sh
@@ -1,30 +1,51 @@
 #!/bin/bash
 ## make sure your bash is after v4.0 for `readarray` to work.
 
-mainDir="/root/curfew" ; mkdir $mainDir/log ; mkdir $mainDir/log/indulgence
 
-# Everything below will go to the file '$logfile':
+
+mainDir="/root/curfew" ; mkdir $mainDir/log $mainDir/var ;
+
+
+
+# LOGGING #
+## Everything below will go to the file '$logfile':
 logfile=$mainDir/log/$(date "+%Y-%m-%dT%H:%M:%S%z").txt
 exec 3>&1 4>&2
 trap 'exec 2>&4 1>&3' 0 1 2 3 RETURN
 exec 1>$logfile 2>&1
 
-# configs
+
+
+# MODULAR CONFIGS #
 ## readarray is supported only after bash 4.0
 readarray -t app_list < $mainDir/config/app_list
 readarray -t user_list < $mainDir/config/user_list
 readarray -t url_list < $mainDir/config/url_list
 
+
+
+# CONFIGS #
 FunTimeStarts=170000 ; FunTimeEnds=173000
 CurfewStarts=174000 ; CurfewEnds=235959
 MidnightWorkStarts=000000 ; MidnightWorkEnds=060000
 
-readarray -t indulgence
-indulgence=600
-indulgenceSession="False"
 
 
-# Auxiliary functions
+# Indulgence Module #
+indulgenceDir="$mainDir/var/indulgence"
+indulgenceTodayFile="$indulgenceDir/$(date +"%Y%m%d").txt"
+indulgenceDefaultAmount=600
+
+mkdir $indulgenceDir ; touch $indulgenceTodayFile
+if [[ -s $indelgenceTodayFile ]]; then
+	echo "message: indulgence file has been here. do nothing."
+else	echo "indulgenceAmount=$indulgenceDefaultAmount" >> "$indulgenceTodayFile"
+	echo "indulgenceMode="off"" >> "$indulgenceTodayFile"
+fi
+
+
+
+# Auxiliary functions #
 	if_within() {
 		now=$(date +"%H%M%S") ; Start=$1 ; End=$2 ;
 		[[ $Start < $End ]] && over_midnight=false || over_midnight=true
@@ -34,7 +55,7 @@ indulgenceSession="False"
 		fi
 	}
 
-# Actions
+# Actions #
 	ban_url() {
 		echo "execute: ban_url"
 		if [[ ${#url_list[@]} != 0 ]] ; then
@@ -83,20 +104,38 @@ indulgenceSession="False"
 		fi
 	}
 
+	toggle_indulgence() {
+		source $indulgenceTodayFile
+		if [[ $indulgenceAmount -lt 0 ]]; then
+			echo "indulgenceMode="off""
+		else
+			[[ $indulgenceMode == "on" ]] && echo "indulgenceMode="off"" || echo "indulgenceMode="on""
+		fi
+	}
 
 
-### MAIN ###
+# MAIN #
 
 main() {
-	while true; do
+	if [[ ! -z $1 ]]; then
+		[[ $1 == "--indulgence" ]] && toggle_indulgence
+		cat $indulgenceTodayFile
+	fi
+
+
+
+	while [[ -z $1 ]]; do 								# While no argument.. do this infinite loop.
 		echo "current: $(date "+%Y-%m-%dT%H:%M:%S%z")"
 		now=$(date +"%H%M%S") ; sleep 5 ;
 
 		# if [[ $(date +%u) -lt 7 ]] ; then 					# restrictions on non-Sundays
 		if [[ $(date +%u) -lt 8 ]] ; then 					# restrictions everyday!
 
-			if [[ $indulgenceSession == "True" ]] ; then
-				echo "message: indulgence-session on; no restrictions impulsed"
+			source $indulgenceTodayFile
+			if [[ $indulgenceMode == "on" ]] ; then
+				echo "message: indulgence-mode on; no restrictions impulsed"
+				echo "indulgenceAmount=$(expr $indulgenceAmount -5)" >> "$indulgenceTodayFile"
+
 			elif $(if_within $FunTimeStarts $FunTimeEnds) ; then		# during fun-time
 				echo "message: fun-time on"
 				unban_url
