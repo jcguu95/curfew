74bb0c2a1eeb61c1431cec6de31c357087531a8d
jin <jcguu95@gmail.com>
2020-10-31 18:06:09 -0500

    change service file name

--- unit/curfew.d
+++ unit/curfew.d
@@ -8,6 +8,6 @@ exec 2> /tmp/rc.local.log	# send stderr from rc.local to a log file
 exec 1>&2			# send stdout to the same log file
 set -x				# tell sh to display commands before execution
 
-bash /root/curfew/main.sh
+bash /root/curfew/main
 
 exit 0
